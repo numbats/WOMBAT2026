2a55c979f149ba5a77dd5e0a077f9729699b2afa
janithwanni <janithcwanni@gmail.com>
2026-08-18 00:01:37 +1000

    add session details

--- program/update_sessions.R
+++ program/update_sessions.R
@@ -1,24 +1,27 @@
 library(httr2)
-req <- request("https://conf.nectric.com.au/api/events/wombat-2026/")
-
-sessions <- req |> 
-  req_url_path_append("schedules/latest/") |> 
-  req_url_query(expand="slots.submission,slots.room") |> 
-  req_perform() |> 
-  resp_body_json()
-
-speakers <- req |> 
-  req_url_path_append("speakers/") |> 
-  req_perform() |> 
-  resp_body_json()
-
-rooms <- req |> 
-  req_url_path_append("rooms/") |> 
-  req_perform() |> 
-  resp_body_json()
-
 library(purrr)
 library(tibble)
+library(dplyr)
+library(tidyr)
+
+req <- request("https://conf.nectric.com.au/api/events/wombat-2026/")
+
+sessions <- req |>
+  req_url_path_append("schedules/latest/") |>
+  req_url_query(expand = "slots.submission,slots.room") |>
+  req_perform() |>
+  resp_body_json()
+
+speakers <- req |>
+  req_url_path_append("speakers/") |>
+  req_perform() |>
+  resp_body_json()
+
+rooms <- req |>
+  req_url_path_append("rooms/") |>
+  req_perform() |>
+  resp_body_json()
+
 recurse_tibble <- function(x) {
   is_list_col <- lengths(x) > 1
   is_df_col <- map_lgl(x, \(y) !is.null(names(y)))
@@ -31,37 +34,36 @@ recurse_tibble <- function(x) {
 }
 
 escape_html <- function(x) {
-  x |> 
-    gsub("&amp;", "&", x=_) |>
-    gsub("&gt;", ">", x=_) |>
-    gsub("&lt;", "<", x=_) |>
-    gsub("&quot;", '"', x=_) |>
-    gsub("&#39;", "'", x=_) |>
-    gsub("<[^>]*>", "", x=_)  # Remove HTML tags
+  x |>
+    gsub("&amp;", "&", x = _) |>
+    gsub("&gt;", ">", x = _) |>
+    gsub("&lt;", "<", x = _) |>
+    gsub("&quot;", '"', x = _) |>
+    gsub("&#39;", "'", x = _) |>
+    gsub("<[^>]*>", "", x = _) # Remove HTML tags
 }
 
-library(dplyr)
-library(tidyr)
-speakers_tidy <- map_dfr(speakers$results,recurse_tibble) |> 
+speakers_tidy <- map_dfr(speakers$results, recurse_tibble) |>
   mutate(biography = escape_html(biography)) |>
-  unnest(submissions) |> 
+  unnest(submissions) |>
   nest(.key = "speakers", .by = submissions)
-sessions_tidy <- map_dfr(sessions$slots,recurse_tibble) |> 
-  filter(!is.na(submission$code)) |> 
+
+sessions_tidy <- map_dfr(sessions$slots, recurse_tibble) |>
+  filter(!is.na(submission$code)) |>
   transmute(
-    start = as.POSIXct(start, format = "%Y-%m-%dT%H:%M:%S+10:00"),
+    start = as.POSIXct(start, format = "%Y-%m-%dT%H:%M:%S+11:00"),
     duration,
     time = format(start, "%B %d, %I:%M %p"),
     title = submission$title |> escape_html(),
     abstract = submission$abstract |> escape_html(),
     submissions = submission$code,
     room = room$name |> unlist()
-  ) |> 
+  ) |>
   left_join(speakers_tidy, by = "submissions")
 
 write_session_qmd <- function(x, ...) {
-  is_tutorial <- x$start < as.POSIXct("2026-09-30")
-  if(is_tutorial) {
+  is_tutorial <- x$start < as.POSIXct("2026-12-01")
+  if (is_tutorial) {
     dir <- "program/tutorials"
     x$register <- "[Register for this tutorial](https://events.humanitix.com/wombat-2026-day-1-tutorials)"
   } else {
@@ -71,21 +73,37 @@ write_session_qmd <- function(x, ...) {
   path <- xfun::with_ext(file.path(dir, x$submissions), "qmd")
 
   x <- as.list(x)
-  
+
   # Generate short summary
-  if(file.exists(path)) {
+  if (file.exists(path)) {
     x$description <- rmarkdown::yaml_front_matter(path)$description
   }
-  if(is.null(x$description)) {
-    chat <- ellmer::chat_google_gemini(
-      system_prompt = "Briefly summarise the key session topics in a plain text from the following abstract. The summary should start with a background details sentence, followed a sentence detailing the key topics of the session in passive voice."
-    )
-    x$description <- chat$chat(x$abstract)
-    # x$description <- ""
+  if (is.null(x$description)) {
+    # chat <- ellmer::chat_google_gemini(
+    #   system_prompt = "Briefly summarise the key session topics in a plain text from the following abstract. The summary should start with a background details sentence, followed a sentence detailing the key topics of the session in passive voice."
+    # )
+    # x$description <- chat$chat(x$abstract)
+    x$description <- "Generated summary"
   }
   x$description <- gsub("\r\n", " ", x$description)
   x$description <- gsub("’", "'", x$description)
-  
+
+  speaker_tbl <- x$speakers[[1]]
+  speaker <- NULL
+  speaker_list <- NULL
+  if (is.null(speaker_tbl)) {
+    speaker <- list(
+      code = "",
+      name = "",
+      avatar_url = ""
+    )
+    speaker_list <- c(" ")
+    x$speakers <- speaker
+  } else {
+    speaker <- transpose(x$speakers[[1]][c("code", "name", "avatar_url")])
+    speaker_list <- paste(x$speakers[[1]][["name"]], collapse = ", ")
+    x$speakers <- transpose(x$speakers[[1]])
+  }
   x$yml <- yaml::as.yaml(
     list(
       pagetitle = paste("WOMBAT 2026:", x$title),
@@ -94,15 +112,15 @@ write_session_qmd <- function(x, ...) {
       title = x$title,
       description = x$description,
       # abstract = x$abstract,
-      speaker = transpose(x$speakers[[1]][c("code", "name", "avatar_url")]),
-      speakerlist = paste(x$speakers[[1]][["name"]], collapse = ", "),
-      room = paste("Room ", x$room, collapse = ' ')
+      speaker = speaker,
+      speakerlist = speaker_list,
+      room = paste("Room ", x$room, collapse = " "),
+      format = list(html = list(css = "../../css/talks.css"))
     )
   )
   x$is_tutorial <- is_tutorial
   x$is_workshop <- !is_tutorial
-  
-  x$speakers <- transpose(x$speakers[[1]])
+
   xfun::write_utf8(
     whisker::whisker.render(
       xfun::read_utf8("program/session_template.qmd"),
@@ -113,6 +131,6 @@ write_session_qmd <- function(x, ...) {
 }
 
 # Workshops
-sessions_tidy |> 
-  rowwise() |> 
+sessions_tidy |>
+  rowwise() |>
   group_walk(write_session_qmd)
